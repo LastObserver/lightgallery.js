8dd80b854dd76846be984461f81f9067557ab074
LastObserver <dirockall@gmail.com>
2018-10-16 16:26:19 +0500

    fix google street view conflict

--- dist/js/lightgallery.js
+++ dist/js/lightgallery.js
@@ -1,5 +1,5 @@
 /**!
- * lightgallery.js | 1.0.3 | August 8th 2018
+ * lightgallery.js | 1.1.1 | October 16th 2018
  * http://sachinchoolur.github.io/lightgallery.js/
  * Copyright (c) 2016 Sachin N; 
  * @license GPLv3 
@@ -22,27 +22,15 @@
     Object.defineProperty(exports, "__esModule", {
         value: true
     });
-
-    /*
-     *@todo remove function from window and document. Update on and off functions
-     */
-    window.getAttribute = function (label) {
-        return window[label];
-    };
-
-    window.setAttribute = function (label, value) {
-        window[label] = value;
-    };
-
-    document.getAttribute = function (label) {
-        return document[label];
-    };
-
-    document.setAttribute = function (label, value) {
-        document[label] = value;
-    };
-
     var utils = {
+        getAttribute: function getAttribute(elem, label) {
+            return elem[label];
+        },
+
+        setAttribute: function setAttribute(elem, label, value) {
+            elem[label] = value;
+        },
+
         wrap: function wrap(el, className) {
             if (!el) {
                 return;
@@ -120,15 +108,17 @@
             uid: 0
         },
         on: function on(el, events, fn) {
+            var _this = this;
+
             if (!el) {
                 return;
             }
 
             events.split(' ').forEach(function (event) {
-                var _id = el.getAttribute('lg-event-uid') || '';
+                var _id = _this.getAttribute(el, 'lg-event-uid') || '';
                 utils.Listener.uid++;
                 _id += '&' + utils.Listener.uid;
-                el.setAttribute('lg-event-uid', _id);
+                _this.setAttribute(el, 'lg-event-uid', _id);
                 utils.Listener[event + utils.Listener.uid] = fn;
                 el.addEventListener(event.split('.')[0], fn, false);
             });
@@ -139,7 +129,7 @@
                 return;
             }
 
-            var _id = el.getAttribute('lg-event-uid');
+            var _id = this.getAttribute(el, 'lg-event-uid');
             if (_id) {
                 _id = _id.split('&');
                 for (var i = 0; i < _id.length; i++) {
@@ -150,14 +140,14 @@
                                 if (utils.Listener.hasOwnProperty(key)) {
                                     if (key.split('.').indexOf(_event.split('.')[1]) > -1) {
                                         el.removeEventListener(key.split('.')[0], utils.Listener[key]);
-                                        el.setAttribute('lg-event-uid', el.getAttribute('lg-event-uid').replace('&' + _id[i], ''));
+                                        this.setAttribute(el, 'lg-event-uid', this.getAttribute(el, 'lg-event-uid').replace('&' + _id[i], ''));
                                         delete utils.Listener[key];
                                     }
                                 }
                             }
                         } else {
                             el.removeEventListener(_event.split('.')[0], utils.Listener[_event]);
-                            el.setAttribute('lg-event-uid', el.getAttribute('lg-event-uid').replace('&' + _id[i], ''));
+                            this.setAttribute(el, 'lg-event-uid', this.getAttribute(el, 'lg-event-uid').replace('&' + _id[i], ''));
                             delete utils.Listener[_event];
                         }
                     }
